d0cc4bc901b329d74802dae35fa74cf741938816
Justin Marshall <justinmarshall20@gmail.com>
2020-12-16 13:43:11 -0800

    Added more pickup meshes.

--- Assets/StreamingAssets/voxels/readme.txt
+++
@@ -1,2 +0,0 @@
-Voxels from:
-https://github.com/NightFright2k19/duke3d_voxelpack
--- Scripts/precache.cs
+++ Scripts/precache.cs
@@ -2,6 +2,11 @@
 {
     private void PrecacheModels()
     {
+        traps.precachemodel(21, "models/pickups/pistol.obj");
+        traps.precachemodel(28, "models/pickups/shotgun.obj");
+        traps.precachemodel(51, "models/pickups/healthbox.obj");
+        traps.precachemodel(52, "models/pickups/healthbottle.obj");
+        traps.precachemodel(53, "models/pickups/firstaid.obj");
         traps.precachemodel(981, "models/enviro/hydrant.obj");
     }
 }
